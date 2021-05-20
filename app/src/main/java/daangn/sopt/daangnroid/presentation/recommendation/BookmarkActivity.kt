25c09ac914d84bb338b28cc51946cce308156ed4
Han Jinhee <h.wlsgml19@gmail.com>
2021-05-21 02:20:37 +0900

    [FEAT] 추춴가게 <-> 저장됨 화면전환

--- app/src/main/java/daangn/sopt/daangnroid/presentation/recommendation/BookmarkActivity.kt
+++ app/src/main/java/daangn/sopt/daangnroid/presentation/recommendation/BookmarkActivity.kt
@@ -3,10 +3,19 @@ package daangn.sopt.daangnroid.presentation.recommendation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import daangn.sopt.daangnroid.R
+import daangn.sopt.daangnroid.databinding.ActivityBookmarkBinding
 
 class BookmarkActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBookmarkBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bookmark)
+        binding = ActivityBookmarkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivBookmarkBack.setOnClickListener{
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/daangn/sopt/daangnroid/presentation/recommendation/RecommendationActivity.kt
+++ app/src/main/java/daangn/sopt/daangnroid/presentation/recommendation/RecommendationActivity.kt
@@ -1,12 +1,34 @@
 package daangn.sopt.daangnroid.presentation.recommendation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import daangn.sopt.daangnroid.R
+import daangn.sopt.daangnroid.databinding.ActivityRecommendationBinding
 
 class RecommendationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRecommendationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recommendation)
+        binding = ActivityRecommendationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bookMarkClickEvent()
+
+        binding.ivRecommendBack.setOnClickListener{
+            onBackPressed()
+        }
+    }
+
+    private fun bookMarkClickEvent(){
+        binding.ivRecommendGoBookmark.setOnClickListener{ startBookmarkActivity() }
+    }
+
+    private fun startBookmarkActivity(){
+        var intent = Intent(this@RecommendationActivity, BookmarkActivity::class.java)
+        startActivity(intent)
+
     }
 }
