9460549587d3e11da1dbcf19ce918f0895b05207
jiwon_ryu <happyjw3910@gmail.com>
2021-05-22 14:14:20 +0900

    News View

--- app/src/main/java/daangn/sopt/daangnroid/presentation/news/NewsActivity.kt
+++ app/src/main/java/daangn/sopt/daangnroid/presentation/news/NewsActivity.kt
@@ -1,37 +1,40 @@
 package daangn.sopt.daangnroid.presentation.news
 
-import android.R
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
+import com.google.android.material.chip.ChipDrawable
+import daangn.sopt.daangnroid.R
 import daangn.sopt.daangnroid.databinding.ActivityNewsBinding
 import daangn.sopt.daangnroid.presentation.news.news.NewsAdapter
 import daangn.sopt.daangnroid.presentation.news.news.NewsInfo
 
 
-class NewsActivity : AppCompatActivity(){
+class NewsActivity : AppCompatActivity() {
     private var _binding: ActivityNewsBinding? = null
-    private val binding get() = _binding?: error("view 참조 실패")
-    private val viewModel : NewsViewModel by viewModels()
+    private val binding get() = _binding ?: error("view 참조 실패")
+    private val viewModel: NewsViewModel by viewModels()
     private lateinit var myAdapter: NewsAdapter
     private lateinit var mLayoutManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("SEARCH_ACTIVITY","created")
+        Log.d("SEARCH_ACTIVITY", "created")
         _binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         init()
         chips()
+        button()
     }
 
-    fun init(){
+    fun init() {
         myAdapter = NewsAdapter()
         binding.rvNews.adapter = myAdapter
         mLayoutManager = LinearLayoutManager(this)
@@ -55,15 +58,50 @@ class NewsActivity : AppCompatActivity(){
         )
     }
 
-    fun chips(){
+    fun chips() {
         val chipGroup = binding.cgGroup
-        val list = listOf("신규","할인","이벤트","카페","네일","강아지","정수기","이사")
-        for(i in list){
-            val chip = Chip(this)
-            chip.text = i
-            //chip.setChipBackgroundColorResource()
-            chipGroup.addView(chip)
-        }
+        val chipResources = listOf("신규", "할인", "이벤트", "카페", "네일", "강아지", "정수기", "이사")
 
+        chipResources
+            .map { it.toChip() }
+            .map {
+                it.apply {
+                    setTextColor(resources.getColor(R.color.chip_text_color, null))
+                    isCheckable = true
+                    isCheckedIconVisible = false
+                    chipIconTint = ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            this@NewsActivity,
+                            R.color.chip_background_color
+                        )
+                    )
+                    setChipStrokeColorResource(R.color.white)
+                    chipStrokeWidth = 1.0F
+                    isChecked = false
+                }
+            }
+            .map {
+                it.apply {
+                    it.setOnCheckedChangeListener { buttonView, checked ->
+                        Log.d("TAG", "$checked")
+                        buttonView.isChecked = checked
+                    }
+                }
+            }
+            .map { chipGroup.addView(it) }
+    }
+
+    private fun String.toChip() = Chip(this@NewsActivity).apply { text = this@toChip }
+
+    fun button() {
+        binding.ivBack.setOnClickListener {
+            //TODO:뒤로가기
+        }
+        binding.ivAlarm.setOnClickListener {
+            //TODO:알람
+        }
+        binding.ivPencil.setOnClickListener {
+            //TODO:글쓰기
+        }
     }
 }
